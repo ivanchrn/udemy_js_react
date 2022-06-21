8ab37ceebb3726c2a1d501f802d7d50a9b983f8c
Ivan <ivans.cornijs@gmail.com>
2022-06-21 14:21:30 +0300

    JS_objects

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hi");
+let numberOfFilms = +prompt("How many films do you saw?", "");
+let lastFilm = prompt("Last film you watched?", "");
+let filmRating = prompt("How do you rate this film?", "");
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[lastFilm] = filmRating;
+
+console.log(personalMovieDB);
